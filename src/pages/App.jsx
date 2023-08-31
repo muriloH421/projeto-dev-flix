399b8b0276adf2964d9b2d86992ec171f7fbd21b
muriloH421 <basiliomurilo421@gmail.com>
2023-08-31 08:20:09 -0300

    projeto inicializado

---
+++ src/pages/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+import logo from "../assets/devflix.png";
+import searchIcon from "../assets/search.svg";
+import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
+import { useState } from "react";
+
+import "./App.css";
+import MovieCard from "../components/movieCard/movieCard";
+import Footer from "../components/footer/footer";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const apiKey = "eb1a598e";
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+    setMovies(data.Search);
+  };
+
+  const handleKeyPress = (e) => {
+    e.key === "Enter" && searchMovies(searchTerm);
+  };
+
+  // fetch(apiUrl)
+  //   .then((Response) => Response.json())
+  //   .then((data) => console.log(data));
+
+  return (
+    <div id="app">
+      <div className="app">
+        <img src={logo} alt="logo devflix"></img>
+      </div>
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyPress}
+          placeholder="pesquise aqui "
+        />
+        <img
+          src={searchIcon}
+          alt="Icone de pesquisa"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key ={movie.imdbID}  movies={movie}/>
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nenhum filme encontrado 😢</h2>
+        </div>
+      )}
+      <Footer link={"https:github.com.br"}>Murilo</Footer>
+    </div>
+  );
+};
+export default App;
