34c89878994b1d69f6f06a33bdc78f5b4af257e3
muriloH421 <basiliomurilo421@gmail.com>
2023-09-01 16:09:26 -0300

    .

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -106,7 +106,7 @@ const App = () => {
           <h2>Nenhum filme encontrado 😢</h2>
         </div>
       )}
-      <Footer link={"https:github.com.br"}>Murilo</Footer>
+      {/* <Footer link={"https:github.com.br"}>Murilo</Footer> */}
     </div>
   );
 };
